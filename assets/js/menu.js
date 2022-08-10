a23802d299a0b403df4c05d5c3032980d45b19e8
dev04jr <110488442+dev04jr@users.noreply.github.com>
2022-08-10 16:45:49 -0400

    del

--- assets/js/menu.js
+++
@@ -1,9 +0,0 @@
-var ul = document.querySelector('nav ul');
-
-function menuShow() {
-    if (ul.classList.contains('open')) {
-        ul.classList.remove('open');
-    } else {
-        ul.classList.add('open');
-    }
-}
